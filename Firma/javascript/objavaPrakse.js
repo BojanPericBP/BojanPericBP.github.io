96f37989e926c1c66b5eb2781a287ec85ab064b9
IvanS99 <ivan.samardzic41@gmail.com>
2021-12-06 13:08:30 +0100

    popravljen jezik od datepicker-a

--- Firma/javascript/objavaPrakse.js
+++ Firma/javascript/objavaPrakse.js
@@ -1,3 +1,22 @@
+//Object.assign(Datepicker.locales, fr);
+
+(function () {
+    Datepicker.locales.en = {
+     
+            days: ["Nedelja", "Ponedeljak", "Utorak", "Srijeda", "Četvratak", "Petak", "Subota"],
+            daysShort: ["Ned", "Pon", "Uto", "Sri", "Čet", "Pet", "Sub"],
+            daysMin: ["Ne", "Po", "Ut", "Sr", "Če", "Pe", "Su"],
+            months: ["Januar", "Februar", "Mart", "April", "Maj", "Jun", "Jul", "Avgust", "Septembar", "Oktobar", "Novembar", "Decembar"],
+            monthsShort: ["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Avg", "Sep", "Okt", "Nov", "Dec"],
+            today: "Danas",
+            clear: "Clear",
+            titleFormat: "MM y",
+            format: "mm/dd/yyyy",
+            weekStart: 0
+
+    }
+}) ();
+
 const getDatePickerTitle = elem => {
     // From the label or the aria-label
     const label = elem.nextElementSibling;
