49f8dc7df7f958b37df6f354ddbed3d3943c465e
IvanS99 <ivan.samardzic41@gmail.com>
2021-12-10 18:16:44 +0100

    objava prakse: validacija neophodnih polja i popravljen layout

--- Firma/javascript/objavaPrakse.js
+++ Firma/javascript/objavaPrakse.js
@@ -1,4 +1,23 @@
 //Object.assign(Datepicker.locales, fr);
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})();
 
 (function () {
     Datepicker.locales.en = {
@@ -36,3 +55,8 @@ for (const elem of elems) {
         title: getDatePickerTitle(elem)
     });
 }
+
+
+function objaviPraksu() {
+    // TODO implement
+}v
