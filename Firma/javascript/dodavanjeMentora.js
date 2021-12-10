b36b7aafd2224228f1038ef6c2134393ea845e60
IvanS99 <ivan.samardzic41@gmail.com>
2021-12-10 18:29:38 +0100

    dodavanje mentora: dodata validacija, ne  funkcioniše sprečavanje zatvaranja forme ako nije validna

--- Firma/javascript/dodavanjeMentora.js
+++ Firma/javascript/dodavanjeMentora.js
@@ -1,3 +1,26 @@
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})();
+
+
+
+
 var popUp = document.getElementById('popUp-Id');
 
 window.onclick = function(event) //kad se klikne u prazno, da nestane popUp
@@ -10,11 +33,13 @@ window.onclick = function(event) //kad se klikne u prazno, da nestane popUp
 
 function closePopUp() 
 {
+    if(document.forms[0].checkValidity)
     popUp.style.display='none';
 }
 
 function showPopUp() 
 {
+   
     popUp.style.display='block';
 }
 
