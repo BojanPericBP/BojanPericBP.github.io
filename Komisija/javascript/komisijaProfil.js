9875f2d6f1e9ed6412824547e10e7e0d97cf4f6b
SasaStankovic <sasa.stankovic.ss@gmail.com>
2022-01-03 18:15:49 +0100

    rijesio problem sa klikom na dugme profil na telefonu

--- Komisija/javascript/komisijaProfil.js
+++ Komisija/javascript/komisijaProfil.js
@@ -1,12 +1,27 @@
 function myFunction() // za otvaranje nav-menija na 3 crtice
 {
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") 
-  {
+  if (x.className === "topnav") {
     x.className += " responsive";
-  } 
-  else 
-  {
+  } else {
     x.className = "topnav";
   }
 }
+
+var x = document.getElementById("dropdown-content-klasa-ID");
+x.style.display = "none";
+
+function profilDropdown() {
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none"
+  }
+}
+
+document.addEventListener('mouseup', function (e) {
+  var dugme = document.getElementById("dropbtn-ID");
+  if (!dugme.contains(e.target)) {
+    x.style.display = 'none';
+  }
+});
